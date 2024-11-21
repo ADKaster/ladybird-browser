cc6ad2c2a25f1dd34a93668f1155eee40b9784b8
Andrew Kaster <andrew@ladybird.org>
2024-11-21 15:11:19 -0700

    Fixup: SwiftSystem

--- Meta/CMake/swift_system.cmake
+++ Meta/CMake/swift_system.cmake
@@ -1,13 +1,16 @@
 include_guard(GLOBAL)
 
 include(FetchContent)
+find_package(Git REQUIRED QUIET)
 
 set(FETCHCONTENT_TRY_FIND_PACKAGE_MODE OPT_IN)
 FetchContent_Declare(SwiftSystem
     GIT_REPOSITORY https://github.com/apple/swift-system.git
     GIT_TAG 1.4.0
-    PATCH_COMMAND "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_LIST_DIR}/patches/git-patch.cmake"
+    PATCH_COMMAND ${GIT_EXECUTABLE} apply --ignore-whitespace
     "${CMAKE_CURRENT_LIST_DIR}/patches/swift-system/0001-CMake-Remove-top-level-binary-module-locations.patch"
+    "${CMAKE_CURRENT_LIST_DIR}/patches/swift-system/0002-CSystem-Set-build-and-install-interfaces-on-included.patch"
+    UPDATE_DISCONNECTED ON
     OVERRIDE_FIND_PACKAGE
 )
 
@@ -16,5 +19,6 @@ set(BUILD_TESTING OFF)
 set(BUILD_EXAMPLES OFF)
 
 FetchContent_MakeAvailable(SwiftSystem)
+install(TARGETS SystemPackage CSystem EXPORT LagomTargets LIBRARY COMPONENT Lagom_Runtime)
 
 set(BUILD_TESTING ${BUILD_TESTING_SAVE})
